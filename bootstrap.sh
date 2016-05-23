6232eaa7dc4a96dbd025bf5a1d2c136a0af5259f
Andrew Beekhof <andrew@beekhof.net>
2016-05-23 16:50:15 +1000

    Updated bootstrap instructions for adding cluster packages to official rhel image

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,16 +7,35 @@ neutron net-create cluster-test --shared --router:external=True --provider:netwo
 export FLOATING_IP_NET="192.0.2"
 neutron subnet-create --name ext-subnet --allocation-pool start=${FLOATING_IP_NET}.50,end=${FLOATING_IP_NET}.64 --disable-dhcp --gateway ${FLOATING_IP_NET}.1 cluster-test "${FLOATING_IP_NET}.0/24"
 
-# https://access.redhat.com/solutions/641193
-# Short version, log in as cloud-user
-# Also useful: https://rwmj.wordpress.com/2015/10/03/tip-updating-rhel-7-1-cloud-images-using-virt-customize-and-subscription-manager/
-wget http://download.eng.bos.redhat.com/brewroot/packages/rhel-guest-image/7.2/20160302.0/images/rhel-guest-image-7.2-20160302.0.x86_64.qcow2
-openstack image create --container-format bare --disk-format qcow2 --public --file rhel-guest-image-7.2-20160302.0.x86_64.qcow2 RHEL72
 
-#wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
-#openstack image create --container-format bare --disk-format qcow2 --public --file CentOS-7-x86_64-GenericCloud.qcow2 CentOS7
+# Either build a new image
 
-#wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
-#openstack image create --container-format bare --disk-format qcow2 --public --file cirros-0.3.4-x86_64-disk.img CirrOS
+if [ -e rhn.pass -a -e rhn.user ]; then
+    #   https://rwmj.wordpress.com/2015/10/03/tip-updating-rhel-7-1-cloud-images-using-virt-customize-and-subscription-manager/
+    image_date=20160302
+    image_version=7.2
 
+    # https://access.redhat.com/solutions/641193
+    # Short version, log in as cloud-user
 
+    wget http://download.eng.bos.redhat.com/brewroot/packages/rhel-guest-image/${image_version}/${image_date}.0/images/rhel-guest-image-${image_version}-${image_date}.0.x86_64.qcow2
+
+    #
+    # Update the image to include required packages (host must be
+    # RHEL73+ or Fedora 22+):
+    #
+   virt-customize  -a rhel-guest-image-${image_version}-${image_date}.0.x86_64.qcow2 \
+      --sm-credentials '$(cat rhn.user):file:rhn.pass' \
+      --sm-register --sm-attach auto \
+      --update
+      --install "pcs,pacemaker,corosync,fence-agents-all,resource-agents,ntp"
+      --sm-unregister 
+
+   openstack image create --container-format bare --disk-format qcow2 --public --file rhel-guest-image-7.2-20160302.0.x86_64.qcow2 ha-guest
+
+else
+    # Create a new one? No success using these images yet
+    virt-builder centos-7.2 --format qcow2 --install "pcs,pacemaker,corosync,fence-agents-all,resource-agents,ntp"
+    openstack image create --container-format bare --disk-format qcow2 --public --file centos-7.2.qcow2 ha-guest
+
+fi
